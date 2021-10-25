d54eb48be1108c429f317ecac22601dea8a21552
Brandon Densmore <densmoreb1@icloud.com>
2021-10-25 11:13:05 -0600

    add list of widgets

--- app/lib/past_workouts.dart
+++ app/lib/past_workouts.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
-import 'exercise.dart';
+import 'workout_card.dart';
 
 class PastWorkouts extends StatelessWidget {
   const PastWorkouts({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-              child: Text('Recent Workouts',
-                  style: TextStyle(fontSize: 30, height: 3),
-                  textAlign: TextAlign.center)),
-          Workout('Bench', 3, 4, 200),
-          Workout('Overhead Press', 3, 6, 115)
-        ]);
+    List<int> loop = [1, 2, 3, 4, 5, 6, 7, 8];
+    return ListView(children: [
+      const SizedBox(
+          child: Text('Recent Workouts',
+              style: TextStyle(fontSize: 30, height: 3),
+              textAlign: TextAlign.center)),
+      for (var i in loop) WorkoutCard('Bench', 3, 4, 200, 'Tuesday'),
+    ]);
   }
 }
-
-// List<Widget> getList() {
-//   List<Workout> list = [];
-//   for (int i = 0; i < 10; i++){
-//     list.add(Workout)
-//   } 
-//   return list;
-//   ];
-// }
-
-
